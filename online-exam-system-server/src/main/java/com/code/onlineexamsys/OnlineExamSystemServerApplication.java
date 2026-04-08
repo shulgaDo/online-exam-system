ee30de27929f1b96208ca268b9b02f4ea48c935a
Shumu <89177879+shulgaDo@users.noreply.github.com>
2026-04-08 20:53:53 +0800

    Initialize a project
    Initialize a project

---
+++ online-exam-system-server/src/main/java/com/code/onlineexamsys/OnlineExamSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.code.onlineexamsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamSystemServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineExamSystemServerApplication.class, args);
+	}
+
+}
---
+++ online-exam-system-server/src/test/java/com/code/onlineexamsys/OnlineExamSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.onlineexamsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamSystemServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
